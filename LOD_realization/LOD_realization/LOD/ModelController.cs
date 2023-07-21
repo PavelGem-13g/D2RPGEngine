1ef6bd4e0628b3cfcd5343b946f1a0286b2e38d5
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2023-07-21 20:58:11 +0300

    add Stride LOD

---
+++ LOD_realization/LOD_realization/LOD/ModelController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Rendering;
+
+namespace LOD_realization.LOD
+{
+    public class ModelController : SyncScript
+    {
+        private Entity _target;
+        private ModelComponent modelComponent;
+        
+        public Model Model1;
+        public Model Model2;
+        public Model Model3;
+        
+        public Entity Target
+        {
+            get
+            {
+                return _target;
+            }
+            
+            set
+            {
+                _target = value;
+            }
+        }
+
+        public override void Start()
+        {
+            modelComponent = Entity.Get<ModelComponent>();
+        }
+
+        public override void Update()
+        {
+            float distance = (_target.Transform.Position-Entity.Transform.Position).Length();
+            DebugText.Print(distance.ToString(), new Int2(100, 100));
+            if(distance < LODSettings.DistanceLow)
+            {
+                modelComponent.Model = Model3;
+            }
+            if(distance < LODSettings.DistanceMiddle)
+            {
+                modelComponent.Model = Model2;
+            }
+            if(distance < LODSettings.DistanceHigh)
+            {
+                modelComponent.Model = Model1;
+            }
+            if(distance > LODSettings.DistanceLow)
+            {
+                modelComponent.Model = null;
+            }
+        }
+    }
+}
---
+++ LOD_realization/LOD_realization/LODSettings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Core;
+using Stride.Core.Annotations;
+
+namespace LOD_realization.LOD
+{
+    public class LODSettings : StartupScript
+    {
+        static LODSettings _instance;
+        
+        [DataMember(1)]
+        [DataMemberRange(0, 100, 1)]
+        public float distanceHigh;
+        [DataMember(2)]
+        [DataMemberRange(0, 100, 1)]
+        public float distanceMiddle;
+        [DataMember(3)]
+        [DataMemberRange(0, 100, 1)]
+        public float distanceLow;
+        
+        public override void Start()
+        {
+            _instance = this;
+        }
+        
+        public static float DistanceHigh
+        {
+            get
+            {
+                return _instance.distanceHigh;
+            }
+            set
+            {
+                _instance.distanceHigh = value;
+            }
+        }
+                
+        public static float DistanceMiddle
+        {
+            get
+            {
+                return _instance.distanceMiddle;
+            }
+            set
+            {
+                _instance.distanceMiddle = value;
+            }
+        }
+               
+        public static float DistanceLow
+        {
+            get
+            {
+                return _instance.distanceLow;
+            }
+            set
+            {
+                _instance.distanceLow = value;
+            }
+        }
+    }
+}
